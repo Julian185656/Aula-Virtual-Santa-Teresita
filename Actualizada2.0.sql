894b7a770d779eff4c38b61865de4e918abbb6b6
KevObando <kobando10466@ufide.ac.cr>
2025-09-30 09:10:56 -0600

    pequeña edicion

--- Actualizada2.0.sql
+++ Actualizada2.0.sql
@@ -1,8 +1,7 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
---
+
 -- Host: 127.0.0.1    Database: aulavirtual
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
